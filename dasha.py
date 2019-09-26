d8d6c41a2216508230bd57a43176e005218cf758
Student <student@client1712.(none)>
2019-09-26 16:42:39 +0300

    dasha.py

--- dasha.py
+++ dasha.py
@@ -1,61 +1,64 @@
 from graph import*
 import math
+
+
+def el(x, y, r, g, b):
+    a = []
+    n = 500
+    for i in range(n):
+        a.append((x +120*math.cos(2*math.pi*i/n), y+30*math.sin(2*math.pi*i/n)))
+        penColor(r, g, b)
+        polygon(a)
+
+def dom(x,y,r,g,b):
+    brushColor(r,g,b)
+    rectangle(20+x, 100+y, 220+x, 450+y)
+    polygon([(10+x, 100+y), (30+x, 80+y), (210+x, 80+y), (230+x, 100+y)])
+    brushColor("#ec8520")
+    rectangle(30+x, 120+y, 50+x, 160+y)
+    rectangle(70+x, 120+y, 90+x, 160+y)
+    rectangle(130+x, 120+y, 150+x, 160+y)
+    rectangle(170+x, 120+y, 190+x, 160+y)
+    rectangle(30+x, 180+y, 50+x, 200+y)
+    rectangle(70+x, 180+y, 90+x, 200+y)
+    rectangle(130+x, 180+y, 150+x, 200+y)
+    rectangle(170+x, 180+y, 190+x, 200+y)
+    brushColor("#f9c56c")
+    rectangle(180+x, 350, 210+x, 400+y)
+    brushColor(49, 0, 98)
+    rectangle(40+x, 350, 70+x, 400+y)
+    rectangle(90+x, 350, 120+x, 400+y)
+
+def ghost(x,y,r,g,b,size):
+    brushColor(r,g,b)
+    polygon([((350+x)*size,( 400+y)*size), ((355+x)*size, (410+y)*size), ((360+x)*size, (405+y)*size), ((365+x)*size,( 410+y)*size), ((370+x)*size,( 407+y)*size),
+((375+x)*size, 403+y*size), (380+x*size, 400+y*size), (385+x*size, 403+y*size), (390+x*size, 400+y*size), (395+x*size, 407+y*size),
+ ((400+x)*size,( 405+y)*size), ((405+x)*size,(407+y)*size),((400+x)*size,(420+y)*size),((395+x)*size,( 390+y)*size),( (393+x)*size,( 393+y)*size), ((390+x)*size, (390+y)*size),
+ ((385+x)*size, (387+y)*size),((380+x)*size, (385+y)*size), ((376+x)*size, (380+y)*size), ((373+x)*size, (376+y)*size), ((370+x)*size,( 360+y)*size),
+ (368+x*size, 345+y*size),
+ ((365+x)*size, (343+y)*size), ((360+x)*size, (340+y)*size), ((357+x)*size, (343+y)*size), ((351+x)*size, (357+y)*size), ((350+x)*size,( 400+y)*size)])
+    brushColor(0,0,0)
+    circle(357+x,350+y,1)
+    circle(364+x,350+y,1)
+
+      
 windowSize(500, 600)
 canvasSize(800, 800)
 brushColor(101, 67, 33)
 rectangle(0, 300, 500, 800)
 brushColor("#c7fcec")
 rectangle(0, 0, 500, 300)
-brushColor(0, 0, 0)
-rectangle(20, 100, 220, 450)
-polygon([(10, 100), (30, 80), (210, 80), (230, 100)])
-brushColor("#ec8520")
-rectangle(30, 120, 50, 160)
-rectangle(70, 120, 90, 160)
-rectangle(130, 120, 150, 160)
-rectangle(170, 120, 190, 160)
-rectangle(30, 180, 50, 200)
-rectangle(70, 180, 90, 200)
-rectangle(130, 180, 150, 200)
-rectangle(170, 180, 190, 200)
-brushColor("#f9c56c")
-rectangle(180, 350, 210, 400)
-brushColor(49, 0, 98)
-rectangle(40, 350, 70, 400)
-rectangle(90, 350, 120, 400)
+dom(0,0,0,0,0)
 brushColor(176, 196, 222)
-polygon([(350, 400), (355, 410), (360, 405), (365, 410), (370, 407), (375, 403), (380, 400), (385, 403), (390, 400), (395, 407),
- (400, 405), (405,407),(400,420),(395, 390), (393, 393), (390, 390), (385, 387),(380, 385), (376, 380), (373, 376), (370, 360), (368, 345),
- (365, 343), (360, 340), (357, 343), (351, 357), (350, 400)])
+ghost(1,1,100,100,100,1)
+ghost(-100,-100,33,232,33,1)
 penColor(255,255,254)
 brushColor(255,255,254)
 circle(460,50,40)
-n = 500
-a=[]
-for i in range(n):
-    x= 250
-    y=50
-    a.append((x+150*math.cos(2*math.pi*i/n), y+30*math.sin(2*math.pi*i/n)))
-    penColor(95, 158, 160)
-    polygon(a)
-n = 500
-a=[]
-for i in range(n):
-    x= 350
-    y=70
-    a.append((x+150*math.cos(2*math.pi*i/n), y+30*math.sin(2*math.pi*i/n)))
-    penColor(176,196,222)
-    polygon(a)
-n = 500
-a=[]
-for i in range(n):
-    x= 370
-    y=170
-    a.append((x+120*math.cos(2*math.pi*i/n), y+30*math.sin(2*math.pi*i/n)))
-    penColor(25,25,112)
-    polygon(a)
+
+el(370,170,0,255,255)
+el(350,70,10,243,30)
+el(250,50,20,255,255)
 penColor(0,0,0)
 brushColor(0,0,0)
-circle(357,350,1)
-circle(364,350,1)
 run()
