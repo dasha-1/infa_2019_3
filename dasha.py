01c9700fd8c751173ef7870e11dc00a0b4167bf3
Student <student@client1712.(none)>
2019-10-03 15:32:13 +0300

    dasha.py

--- dasha.py
+++ dasha.py
@@ -31,15 +31,14 @@ def dom(x,y,r,g,b):
 
 def ghost(x,y,r,g,b,size):
     brushColor(r,g,b)
-    polygon([((350+x)*size,( 400+y)*size), ((355+x)*size, (410+y)*size), ((360+x)*size, (405+y)*size), ((365+x)*size,( 410+y)*size), ((370+x)*size,( 407+y)*size),
+    a =  polygon([((350+x)*size,( 400+y)*size), ((355+x)*size, (410+y)*size), ((360+x)*size, (405+y)*size), ((365+x)*size,( 410+y)*size), ((370+x)*size,( 407+y)*size),
 ((375+x)*size, 403+y*size), (380+x*size, 400+y*size), (385+x*size, 403+y*size), (390+x*size, 400+y*size), (395+x*size, 407+y*size),
  ((400+x)*size,( 405+y)*size), ((405+x)*size,(407+y)*size),((400+x)*size,(420+y)*size),((395+x)*size,( 390+y)*size),( (393+x)*size,( 393+y)*size), ((390+x)*size, (390+y)*size),
  ((385+x)*size, (387+y)*size),((380+x)*size, (385+y)*size), ((376+x)*size, (380+y)*size), ((373+x)*size, (376+y)*size), ((370+x)*size,( 360+y)*size),
  (368+x*size, 345+y*size),
  ((365+x)*size, (343+y)*size), ((360+x)*size, (340+y)*size), ((357+x)*size, (343+y)*size), ((351+x)*size, (357+y)*size), ((350+x)*size,( 400+y)*size)])
-    brushColor(0,0,0)
-    circle(357+x,350+y,1)
-    circle(364+x,350+y,1)
+    return a
+    
 
       
 windowSize(500, 600)
@@ -48,17 +47,34 @@ brushColor(101, 67, 33)
 rectangle(0, 300, 500, 800)
 brushColor("#c7fcec")
 rectangle(0, 0, 500, 300)
+
 dom(0,0,0,0,0)
 brushColor(176, 196, 222)
-ghost(1,1,100,100,100,1)
-ghost(-100,-100,33,232,33,1)
+a= ghost(-100,-100,33,232,33,1)
 penColor(255,255,254)
 brushColor(255,255,254)
 circle(460,50,40)
-
 el(370,170,0,255,255)
 el(350,70,10,243,30)
 el(250,50,20,255,255)
 penColor(0,0,0)
 brushColor(0,0,0)
+c=circle(257,250,1)
+s=circle(264,250,1)
+
+
+def update():
+    moveObjectBy(a, 1, 1)
+    moveObjectBy(c,1,1)
+    moveObjectBy(s,1,1)
+    l=0
+    changeFillColor(a,"grey")
+    l=l+1
+    if xCoord(a)>=500:
+        close()
+   
+
+
+onTimer(update,50)
+
 run()
